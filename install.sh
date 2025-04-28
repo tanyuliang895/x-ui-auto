101e3e25ca553e49ecd6bf81f73b8fa60f3b3271
tanyuliang895 <tanyuliang895@gmail.com>
2025-04-29 00:59:06 +0800

    install.sh

--- install.sh
+++ install.sh
@@ -1,34 +1,23 @@
 #!/bin/bash
 
-# 更新系统
-echo "正在更新系统..."
+# 更新系统并安装必要工具
 apt update -y && apt upgrade -y
-
-# 安装必备工具
-echo "安装curl和wget..."
 apt install curl wget -y
 
 # 下载并安装x-ui
-echo "正在下载并安装x-ui..."
 curl -s https://raw.githubusercontent.com/vaxilu/x-ui/master/install.sh | bash
 
-# 配置x-ui面板
-echo "正在配置x-ui账号、密码和端口..."
-
-# 设置账号和密码
+# 配置x-ui账号、密码和端口
 x-ui account add liang liang
-
-# 修改端口为2024
 sed -i 's/"port": 2020/"port": 2024/' /etc/x-ui/x-ui.json
 
-# 启动x-ui面板
-echo "正在启动x-ui面板..."
+# 启动并设置开机自启
 systemctl enable x-ui
 systemctl start x-ui
 
-# 完成安装并输出信息
-echo "x-ui安装完成！"
+# 完成
+echo "安装完成！"
 echo "账号：liang"
 echo "密码：liang"
 echo "端口：2024"
-echo "访问面板地址： http://<服务器IP>:2024"
+echo "访问面板： http://<服务器IP>:2024"
